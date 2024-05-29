b4a59bc7beca6c821efb6c9ed9fb9ed900c28fe0
Orea <34584301+Choo-Choo-Oreo@users.noreply.github.com>
2024-05-29 00:31:51 -0400

    Strategic View+Commanders Limit

---
+++ common/defines/zz_military_commanders_defines.lua
@@ -0,0 +1,4 @@
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 14 			-- how many divisions a corps commander is limited to. 0 = inf  < 0 = blocked
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 3 	-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked	
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 14 			-- how many divisions a field marshall is limited to. 0 = inf  < 0 = blocked
+NDefines.NPolitics.ARMY_LEADER_COST = 3	 --command power cost for recruiting new leaders, 'this value' * number_of_existing_leaders_of_type	
